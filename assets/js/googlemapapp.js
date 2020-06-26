95326ddc415ca5ea6ff980d42cc90661a1045781
Machiko Lacey <machiko_kimura_lacey@yahoo.co.uk>
2020-06-26 23:44:46 +0100

    Added Cafe data and Fixed issues around button spacing

--- assets/js/googlemapapp.js
+++ assets/js/googlemapapp.js
@@ -47,21 +47,21 @@ function checkBrowserSize(){
     switch(currentcategory){
       case 0 :
         zoom = 14;
-        lat = 50.827317; 
-        lng = -0.133693;
+        lat = 50.833878,
+        lng =   -0.170442;
       break;
       case 1 :
-      zoom = 13;
+      zoom = 14;
       lat = 50.824873;
       lng = -0.126048;
       break;
       case 2 :
-      zoom = 12;
-      lat = 50.864671;
-      lng = -0.169750;
+      zoom = 13;
+      lat = 50.824873;
+      lng = -0.126048;
       break;
       case 3:
-      zoom = 11;
+      zoom = 14;
       lat = 50.842827;
       lng = -0.148028;
       break;
@@ -83,7 +83,12 @@ function checkBrowserSize(){
   function filterCategory(category){
     // doument.getElementsByClassName('category').removeClass('active');
    currentcategory = category;
-   locations = orglocations.filter( i => i.category == category );
+   
+   if(category == 0){
+    locations = orglocations;
+   }else{
+    locations = orglocations.filter( i => i.category == category );
+   }
    //document.getElementById('category'+category).addClass('active');
    console.log(locations);
    initialize();
@@ -119,18 +124,19 @@ var map;
 
 function updateTour(){
 let text = yourtourids.map((item) => {
-  return '<li>'+orglocations[item]["title"] + "<button class='btn btn-danger btn-sm remove-from-tour' destid='"+orglocations[item]["id"]+"' onclick='removeLocation("+item+")'>Remove</button></li>";
+  return '<li>'+orglocations[item]["title"] + "<button class='btn btn-danger btn-sm remove-from-tour' destid='"+orglocations[item]["id"]+"' onclick='removeLocation("+item+")'>Remove</button> </li>";
 }).join(' ');
-let text1 = "<h2>Your Tour</h2><ol>"+text+"</ol> ";
+let text1 = "<ol id='tourlist'>"+text+"</ol> ";
  let text2 = "";
 
  if(yourtourids.length > 1){
   let e = document.createElement('div');
-  text2  = "<button id='check-distance' class='btn btn-secondary' onclick='checkDistance()'>Check Distance</button>";
+  text2  = "<button id='check-distance' class='btn btn-secondary d-inline-block' onclick='checkDistance()'>Check Distance</button>";
+  text2 += "  <button id='reset-tour' class='btn btn-secondary d-inline-block' onclick='resetTour()'>Reset Tour</button>";
  }
 
  document.getElementById("yourtour").innerHTML =  text1 + text2; 
- document.getElementById("tourcontent").style.display = "block";
+ //document.getElementById("tourcontent").style.display = "block";
  
 }
 /*
@@ -158,6 +164,7 @@ function addLocation(id){
       }
 
    updateTour();
+  document.getElementById('direction-panel-modal').innerHTML = ""; 
   document.getElementById("printModal").style.display="none";
 
 }
@@ -168,7 +175,6 @@ function resetTour(){
   // resetDistance();
    initialize();
    document.getElementById("yourtour-panel").style.display="none";
-   document.getElementById("tourcontent").style.display="none";
    document.getElementById("resetpanels").style.display="none";
 }
 
@@ -273,18 +279,18 @@ function setMarkers(map,locations){
     let photo = "";
 
     if(locations[i]['youtube'] != undefined ){    
-        embed = "<iframe width='100%' height='200' src='https://www.youtube.com/embed/"+locations[i]['youtube']+"' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>";
+        embed = "<iframe width='100%' height='280' src='https://www.youtube.com/embed/"+locations[i]['youtube']+"' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>";
     }
     if(locations[i]['youtube'] == undefined && locations[i]['photo'] != undefined){    
       photo = "<img src='assets/images/"+locations[i]['photo']+"' alt='"+locations[i]['title']+"' class='location-photo' />"
     }
-    let button = "<button class='btn btn-primary add-to-tour' id='add-to-yourtour' onclick='addLocation("+locations[i]['id']+")'>Add to your tour</button>";
+    let button = "<button class='btn btn-primary add-to-tour d-inline-block' id='add-to-yourtour' onclick='addLocation("+locations[i]['id']+")'>Add to your tour</button>";
     let currentlocation;
 
      var markercontent = "<h3>" + locations[i]['title'] +  "</h3>";   
      var title =  locations[i]['title'] ;
          content = "</h3><div class='row'><div class='col-sm-6'>"+photo + embed + "</div><div class='col-sm-6'>"
-        + locations[i]['description'] + button + "</div></div>"  ;
+        + locations[i]['description']  + "</div></div>"  ;
 
           let icon = markerIcon;            
           switch(locations[i]['category']){
@@ -305,6 +311,7 @@ function setMarkers(map,locations){
                      position: latlngset  , 
                      icon: icon,
                      content:content,
+                     button:button,
                      optimized: false
             });
             markers.push(marker);
@@ -317,6 +324,7 @@ function setMarkers(map,locations){
                       console.log('id='+marker.id);
                             document.getElementById('direction-panel-modal').innerHTML = marker.content; 
                             document.getElementById('yourtourtitle-modal').innerHTML = marker.title; 
+                            document.getElementById('addbtn-wrapper').innerHTML = marker.button; 
                             if(yourtourids.includes(marker.id)){
                                 document.getElementById('add-to-yourtour').disabled=true;
                                 document.getElementById('add-to-yourtour').innerHTML="You've already added this!";

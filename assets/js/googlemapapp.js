35146dfa55da7dc21564d54653e6002598576ca0
Machiko Lacey <machiko_kimura_lacey@yahoo.co.uk>
2020-06-27 02:00:17 +0100

    Added functionality to reorder tour destinations

--- assets/js/googlemapapp.js
+++ assets/js/googlemapapp.js
@@ -81,66 +81,81 @@ function setMarkers(map,locations){
 
     for (i = 0; i < locations.length; i++)
     {  
-          let location = locations [i]
-          let id      = 0;
-          let embed   = "";
-          let photo = "";
+      let location = locations [i]
+      let id      = 0;
+      let embed   = "";
+      let photo = "";
 
-          if(locations[i]['youtube'] != undefined ){    
-              embed = "<iframe width='100%' height='280' src='https://www.youtube.com/embed/"+locations[i]['youtube']+"' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>";
-          }
-          if(locations[i]['youtube'] == undefined && locations[i]['photo'] != undefined){    
-            photo = "<img src='assets/images/"+locations[i]['photo']+"' alt='"+locations[i]['title']+"' class='location-photo' />"
-          }
-          let button = "<button class='btn btn-primary add-to-tour d-inline-block' id='add-to-yourtour' onclick='addLocation("+locations[i]['id']+")'>Add to your tour</button>";
-          let currentlocation;
+      if(locations[i]['youtube'] != undefined ){    
+          embed = "<iframe width='100%' height='280' src='https://www.youtube.com/embed/"+locations[i]['youtube']+"' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>";
+      }
+      if(locations[i]['youtube'] == undefined && locations[i]['photo'] != undefined){    
+        photo = "<img src='assets/images/"+locations[i]['photo']+"' alt='"+locations[i]['title']+"' class='location-photo' />"
+      }
+      let button = "<button class='btn btn-primary add-to-tour d-inline-block' id='add-to-yourtour' onclick='addLocation("+locations[i]['id']+")'>Add to your tour</button>";
+      let currentlocation;
 
-          let markercontent = "<h3>" + locations[i]['title'] +  "</h3>";   
-          let title =  locations[i]['title'] ;
-              content = "</h3><div class='row'><div class='col-sm-6'>"+photo + embed + "</div><div class='col-sm-6'>"
-              + locations[i]['description']  + "</div></div>"  ;
+      let markercontent = "<h3>" + locations[i]['title'] +  "</h3>";   
+      let title =  locations[i]['title'] ;
+          content = "</h3><div class='row'><div class='col-sm-6'>"
+           +photo + embed + "</div><div class='col-sm-6'>"
+          + locations[i]['description']  + "</div></div>"  ;
 
-          latlngset = new google.maps.LatLng(locations[i]['lat'], locations[i]['lng']);
-          let catdata = categories[locations[i]['category']];
-          let markerIcon = {
-            url: catdata['markerIcon'],
-            scaledSize: new google.maps.Size(catdata['scaledSizeWidth'], catdata['scaledSizeHeight']),
-            origin: new google.maps.Point(catdata['originX'], catdata['originY']),
-            anchor: new google.maps.Point(catdata['anchorX'], catdata['anchorY'])
-          };
-          let marker = new google.maps.Marker({  
-                  id:locations[i]['id'],  
-                  map: map,
-                    title: locations[i]['title'],
-                    position: latlngset  , 
-                    icon: markerIcon,
-                    content:content,
-                    button:button,
-                    optimized: false
-            });
-            markers.push(marker);
-            map.setCenter(marker.getPosition())
+      latlngset = new google.maps.LatLng(locations[i]['lat'], locations[i]['lng']);
+      let catdata = categories[locations[i]['category']];
+      let markerIcon = {
+        url: catdata['markerIcon'],
+        scaledSize: new google.maps.Size(catdata['scaledSizeWidth'], catdata['scaledSizeHeight']),
+        origin: new google.maps.Point(catdata['originX'], catdata['originY']),
+        anchor: new google.maps.Point(catdata['anchorX'], catdata['anchorY'])
+      };
+      let marker = new google.maps.Marker({  
+              id:locations[i]['id'],  
+              map: map,
+                title: locations[i]['title'],
+                position: latlngset  , 
+                icon: markerIcon,
+                content:content,
+                button:button,
+                optimized: false
+        });
+        markers.push(marker);
+        map.setCenter(marker.getPosition())
 
-            google.maps.event.addListener(marker,'click', (function(marker,markercontent, position, content, title, id){ 
-            return function() {
-                  console.log('id='+marker.id);
-                  document.getElementById('direction-panel-modal').innerHTML = marker.content; 
-                  document.getElementById('yourtourtitle-modal').innerHTML = marker.title; 
-                  document.getElementById('addbtn-wrapper').innerHTML = marker.button; 
+        google.maps.event.addListener(marker,'click', (function(marker,markercontent, position, content, title, id){ 
+          return function() {
+                console.log('id='+marker.id);
+                document.getElementById('direction-panel-modal').innerHTML = marker.content; 
+                document.getElementById('yourtourtitle-modal').innerHTML = marker.title; 
+                document.getElementById('addbtn-wrapper').innerHTML = marker.button; 
                   if(yourtourids.includes(marker.id)){
                       document.getElementById('add-to-yourtour').disabled=true;
                       document.getElementById('add-to-yourtour').innerHTML="You've already added this!";     
                   }
-                 document.getElementById('printModal').style.display='block';
-                  currentlocation = marker.id;
-                 };
-                })(marker,markercontent,infowindow)); 
-    } 
+                document.getElementById('printModal').style.display='block';
+                currentlocation = marker.id;
+                };
+            })(marker,markercontent,infowindow)); 
+      } 
 }
 
 function updateTour(){
-    let text = yourtourids.map((item) => {
-      return '<li>'+orglocations[item]["title"] + "<button class='btn btn-danger btn-sm remove-from-tour' destid='"+orglocations[item]["id"]+"' onclick='removeLocation("+item+")'>Remove</button> </li>";
+    let text = yourtourids.map((item,i, arr) => {
+      let downArrow = upArrow ="";
+
+      if(i > 0)
+      {
+         upArrow =  "<span id='plus' onclick='reorderPlus("+i+")'><i class='fa fa-arrow-up' aria-hidden='true'></i></span>";
+      }
+      if(arr.length - 1 !== i){
+         downArrow = "<span id='minus' onclick='reorderMinus("+i+")'><i class='fa fa-arrow-down' aria-hidden='true'></i></span>";
+      }
+
+      return '<li>'+orglocations[item]["title"] 
+      + "<button class='btn btn-danger btn-sm remove-from-tour' destid='"
+      + orglocations[item]["id"]+"' onclick='removeLocation("+item+")'>Remove</button>"
+      +  upArrow  + downArrow +" </li>";
+    
     }).join(' ');
 
     let text1 = "<ol id='tourlist'>"+text+"</ol> ";
@@ -154,6 +169,25 @@ function updateTour(){
 
     document.getElementById("yourtour").innerHTML =  text1 + text2; 
 }
+
+function reorderPlus(id){
+console.log(id);
+console.log(yourtourids);
+x= id, y= id-1;
+yourtourids[x] = yourtourids.splice(y, 1, yourtourids[x])[0];
+console.log(yourtourids);
+updateTour();
+}
+
+
+function reorderMinus(id){
+  console.log(id);
+console.log(yourtourids);
+x= id, y= id+1;
+yourtourids[x] = yourtourids.splice(y, 1, yourtourids[x])[0];
+console.log(yourtourids);
+updateTour();
+  }
 
 function distancCalculator(id, key){  
   let nextid = yourtourids[key+1];
